c1989a69df1fbc214ba63839a49c2ee887e7f3b3
Ramino Jake Santos <xsantosx10@gmail.com>
2022-02-07 12:07:33 +0800

    Update home.php

--- home.php
+++ home.php
@@ -10,8 +10,8 @@
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link rel = "stylesheet" type = "text/css" href="css/style1.css" media="all">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="js/bootstrap.js"></script>
 	<script src="js/jquery-1.7.2.min.js"></script>
 	<script src="js/carousel.js"></script>
@@ -94,11 +94,11 @@
 	<div class="nav">
 				<ul>
 					<li><a href="index.php"><i class="icon-home"></i>Home</a></li>
-					<li><a href="product.php"><i class="icon-th-list"></i>Product</a>
-					<li><a href="aboutus.php"><i class="icon-bookmark"></i>About Us</a></li>
-					<li><a href="contactus.php"><i class="icon-inbox"></i>Contact Us</a></li>
-					<li><a href="privacy.php"><i class="icon-info-sign"></i>Privacy Policy</a></li>
-					<li><a href="faqs.php"><i class="icon-question-sign"></i>FAQs</a></li>
+					<li><a href="product1.php"><i class="icon-th-list"></i>Product</a>
+					<li><a href="aboutus1.php"><i class="icon-bookmark"></i>About Us</a></li>
+					<li><a href="contactus1.php"><i class="icon-inbox"></i>Contact Us</a></li>
+					<li><a href="privacy1.php"><i class="icon-info-sign"></i>Privacy Policy</a></li>
+					<li><a href="faqs1.php"><i class="icon-question-sign"></i>FAQs</a></li>
 				</ul>
 			</div>
 <div id="container">
